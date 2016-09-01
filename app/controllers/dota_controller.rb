fbf3f99afdabc019e7bb7f89c3806e0ecdc5f56f
Sn8z <marcus.sneitz@telia.com>
2016-09-01 17:01:02 +0200

    new visuals

--- app/controllers/dota_controller.rb
+++ app/controllers/dota_controller.rb
@@ -1,5 +1,14 @@
 class DotaController < ApplicationController
   def index
+
+    require 'open-uri'
+    @highlights = []
+    JSON.parse(open("https://api.twitch.tv/kraken/videos/top?game=Dota+2&period=month&limit=100").read)["videos"].each do |highlight|
+      if highlight["language"].eql?("en")
+        @highlights.push(highlight)
+      end
+    end
+
     api = Dota.api
     @live_matches = []
     api.live_matches.each do |match|
