ec11d9c52e546922270c706150da23929e2dc087
Sn8z <marcus.sneitz@telia.com>
2016-09-29 12:53:39 +0200

    landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @news = Game.find_by(:name => 'Gaming').news.last(5)
     @reddit = Game.find_by(:name => 'Gaming').reddits.last(5)
     @streamers = Game.find_by(:name => 'Gaming').streamers.last(6)
-    @youtube = Youtube.last(3).reverse
   end
 end
