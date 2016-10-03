2d8a4568bf04734f33da685c8842493cace2d043
Sn8z <marcus.sneitz@telia.com>
2016-10-03 13:03:59 +0200

    moved images

--- app/controllers/csgo_controller.rb
+++ app/controllers/csgo_controller.rb
@@ -3,7 +3,7 @@ class CsgoController < ApplicationController
         @news = Game.find_by(:name => 'Counterstrike Global Offensive').news.last(5)
         @reddit = Game.find_by(:name => 'Counterstrike Global Offensive').reddits.last(5)
         @streamers = Game.find_by(:name => 'Counterstrike Global Offensive').streamers.last(6)
-        @youtube = Game.find_by(:name => 'Counterstrike Global Offensive').youtubes.last(3).reverse
+        @youtube = Game.find_by(:name => 'Counterstrike Global Offensive').youtubes.last(4).reverse
         @guides = Game.find_by(:name => 'Counterstrike Global Offensive').guides.last(5).reverse
         @headlines = Game.find_by(:name => 'Counterstrike Global Offensive').headlines.last(4).reverse
     end
