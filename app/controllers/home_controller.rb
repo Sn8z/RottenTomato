ca8e1300df9439507c927489fd477db0a2fb635b
Sn8z <marcus.sneitz@telia.com>
2016-11-14 12:29:47 +0100

    updated headlines and some CSS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @reddit = Game.find_by(:name => 'Gaming').reddits.last(5)
     @streamers = Game.find_by(:name => 'Gaming').streamers.last(6)
-    @headlines = Headline.last(3).reverse
+    @headlines = Headline.last(5).reverse
   end
 end
